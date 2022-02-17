7c22fc0885d0335150e526686aabe14ef5b67ecc
EvelynChow <90713511+EvelynChow@users.noreply.github.com>
2022-02-17 10:28:07 -0500

    first

---
+++ .eleventy.js
@@ -0,0 +1,11 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassThroughCopy("src/style");
+    eleventyConfig.addPassThroughCopy("src/md");
+    eleventyConfig.addPassThroughCopy("src/media");
+    return{
+        dir:{
+            input:"src",
+            output:"dist",
+        }
+    }
+}
