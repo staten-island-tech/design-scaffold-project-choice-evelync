dd7e0eb26460ebd7b110c3ab38c3c2904cefd7e6
EvelynChow <90713511+EvelynChow@users.noreply.github.com>
2022-04-01 10:56:37 -0400

    twenty second

--- .eleventy.js
+++ .eleventy.js
@@ -4,6 +4,7 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/md");
     eleventyConfig.addPassthroughCopy("src/media");
     eleventyConfig.addPassthroughCopy("src/admin");
+    eleventyConfig.addPassthroughCopy("src/index.js");
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     });
